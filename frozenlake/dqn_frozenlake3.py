050062b0bedf08753e144e75006b800a025ce408
Brian Crafton <crafton.b@husky.neu.edu>
2018-05-10 22:02:37 -0400

    summer 18

---
+++ frozenlake/dqn_frozenlake3.py
@@ -0,0 +1,154 @@
+# Inspired by https://keon.io/deep-q-learning/
+
+import random
+import gym
+import math
+import numpy as np
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+
+def get_reward(done, reward):
+    """
+    Return a more meaningful reward (i.e. depending on the outcome of every action and not just on winning)
+    :param done: bool indicating if the episode if finished
+    :param reward: reward returned by the env after taking an action
+    :return:
+    """
+    # Failure
+    if done and reward == 0:
+        reward = - 100.0
+    # Win
+    elif done:
+        reward = 100.0
+    # Move to another case
+    else:
+        reward = - 4.0
+    return reward
+
+class DQNCartPoleSolver():
+    def __init__(self, n_episodes=10000, max_env_steps=None, gamma=1.0, epsilon=0.5, epsilon_min=0.01, epsilon_decay=0.99, alpha=0.1, alpha_decay=0.01, batch_size=32, quiet=False):
+        self.memory = deque(maxlen=32)
+
+        # self.env = gym.make('FrozenLake-v0')
+        self.env = gym.make('FrozenLake8x8-v0')
+
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_decay
+        self.alpha = alpha
+        self.alpha_decay = alpha_decay
+        self.n_episodes = n_episodes
+        self.batch_size = batch_size
+        self.quiet = quiet
+        self.decay_step = 10
+        if max_env_steps is not None: self.env._max_episode_steps = max_env_steps
+
+        # Init model
+        self.model = Sequential()
+
+        # self.model.add(Dense(4, input_dim=16, activation='linear'))
+
+        # self.model.add(Dense(32, input_dim=16, activation='linear'))
+        # self.model.add(Dense(4, activation='linear'))
+
+        self.model.add(Dense(4, input_dim=64, activation='linear'))
+
+        self.model.compile(loss='mse', optimizer=Adam(lr=self.alpha))
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def choose_action(self, state, epsilon):
+        if (np.random.random() <= epsilon):
+            ret = self.env.action_space.sample()
+        else:
+            # argmax is different than max.
+            ret = np.argmax(self.model.predict(state))
+
+        # print (ret)
+        # ret = self.env.action_space.sample() if (np.random.random() <= epsilon) else np.argmax(self.model.predict(state))
+        # print (ret)
+
+        return ret
+
+    def get_epsilon(self, t):
+        return max(self.epsilon_min, min(self.epsilon, 1.0 - math.log10((t + 1) * self.epsilon_decay)))
+
+    def preprocess_state(self, state):
+        # return np.reshape(state, [1, 4])
+        # return state
+        # return np.reshape(state, [1, 1])
+
+        '''
+        ret = np.zeros(16)
+        for i in range(16):
+            if state == i:
+                ret[i] = 1
+        return np.reshape(ret, [1, 16])
+        '''
+        ret = np.zeros(64)
+        for i in range(64):
+            if state == i:
+                ret[i] = 1
+        return np.reshape(ret, [1, 64])
+
+    def replay(self, batch_size):
+        x_batch, y_batch = [], []
+        minibatch = random.sample(self.memory, min(len(self.memory), batch_size))
+
+        for state, action, reward, next_state, done in minibatch:
+            y_target = self.model.predict(state)
+            y_target[0][action] = reward if done else reward + self.gamma * np.max(self.model.predict(next_state))
+            x_batch.append(state[0])
+            y_batch.append(y_target[0])
+
+            # print (np.average(y_target[0]))
+        
+            # print (len(self.model.predict(next_state)))
+            # print (np.max(self.model.predict(next_state)[0]))
+            # print (np.max(self.model.predict(next_state)))
+
+        self.model.fit(np.array(x_batch), np.array(y_batch), batch_size=len(x_batch), verbose=0)
+
+    def run(self):
+        scores = deque(maxlen=100)
+
+        for e in range(self.n_episodes):
+            state = self.env.reset()
+            state = self.preprocess_state(state)
+
+            # print (self.env.spec.timestep_limit)
+            for step in range(self.env.spec.timestep_limit):
+                # print (state.shape())
+                action = self.choose_action(state, self.get_epsilon(e))
+
+                next_state, reward, done, _ = self.env.step(action)
+                next_state = self.preprocess_state(next_state)
+
+                reward = get_reward(done, reward)
+
+                self.remember(state, action, reward, next_state, done)
+
+                state = next_state
+
+                if done:
+                    self.replay(self.batch_size)
+
+                    if self.epsilon > self.epsilon_min:
+                        self.epsilon *= self.epsilon_decay ** (e / self.decay_step)
+
+                    scores.append(reward > 0)
+                    mean_score = np.mean(scores)
+
+                    print (mean_score)
+                    # print (self.get_epsilon(e))
+
+                    break
+        
+
+if __name__ == '__main__':
+    agent = DQNCartPoleSolver()
+    agent.run()
---
+++ stdp2layer/stdp2layer_cuda.py
@@ -0,0 +1,153 @@
+from brian2 import *
+import brian2cuda
+set_device("cuda_standalone")
+
+import numpy as np
+import cPickle, gzip, os, sys, signal
+
+def load_training_set():
+  global training_set, training_labels
+  f = gzip.open('mnist.pkl.gz', 'rb')
+  train, valid, test = cPickle.load(f)
+
+  [training_set, training_labels] = train
+  [validation_set, validation_labels] = valid
+  [testing_set, testing_labels] = test
+
+  for i in range( len(training_set) ):
+    training_set[i] = training_set[i].reshape(28*28)
+    training_set[i] = training_set[i] / np.max(training_set[i])
+    training_set[i] = training_set[i]
+
+  f.close()
+
+def spike_plot(spike_mon):
+  spike_plot_x = []
+  spike_plot_y = []
+  trains = spike_mon.spike_trains()
+  for i in trains:
+    spike_plot_x.extend(trains[i])
+
+    sz = len(trains[i])
+    vals = [i] * sz
+    spike_plot_y.extend( vals )
+  
+  return spike_plot_x, spike_plot_y
+
+taum = 10*ms
+taupre = 20*ms
+taupost = 20*ms
+
+Ee = 0*mV
+vt = -54*mV
+vr = -60*mV
+El = -74*mV
+taue = 5*ms
+
+F = 15*Hz
+gmax = .01
+
+dApre = .01
+dApost = -dApre * (taupre / taupost) * 1.05
+dApost *= gmax
+dApre *= gmax
+
+eqs_neurons = '''
+dv/dt = (ge * (Ee-vr) + El - v) / taum : volt
+dge/dt = -ge / taue : 1
+'''
+
+input = PoissonGroup(28*28, rates=F)
+spike_mon0 = SpikeMonitor(input)
+
+neurons = NeuronGroup(10*10, eqs_neurons, threshold='v>vt', reset='v = vr', method='exact')
+output = NeuronGroup(4*4, eqs_neurons, threshold='v>vt', reset='v = vr', method='exact')
+
+###########################
+
+S0 = Synapses(input, neurons,
+             '''w : 1
+                dApre/dt = -Apre / taupre : 1 (event-driven)
+                dApost/dt = -Apost / taupost : 1 (event-driven)''',
+             on_pre='''
+                    ge += w
+                    Apre += dApre
+                    w = clip(w + Apost, 0, gmax)''',
+             on_post='''
+                    Apost += dApost
+                    w = clip(w + Apre, 0, gmax)''',
+             )
+S0.connect()
+S0.w = 'rand() * gmax'
+
+# pretty sure [0,1] is the range of the weight to record.
+# mon = StateMonitor(S0, 'w', record=[0, 1])
+# NOOO [0,1] was actually record index 0 and 1...
+# so we were just looking at indexes 0 and 1.
+
+mon0 = StateMonitor(S0, 'w', record=range(10*10))
+spike_mon1 = SpikeMonitor(neurons)
+
+###########################
+
+S1 = Synapses(neurons, output,
+             '''w : 1
+                dApre/dt = -Apre / taupre : 1 (event-driven)
+                dApost/dt = -Apost / taupost : 1 (event-driven)''',
+             on_pre='''
+                    ge += w
+                    Apre += dApre
+                    w = clip(w + Apost, 0, 0.1)''',
+             on_post='''
+                    Apost += dApost
+                    w = clip(w + Apre, 0, 0.1)''',
+             )
+S1.connect()
+S1.w = 'rand() * 0.1'
+
+# pretty sure [0,1] is the range of the weight to record.
+# mon = StateMonitor(S0, 'w', record=[0, 1])
+# NOOO [0,1] was actually record index 0 and 1...
+# so we were just looking at indexes 0 and 1.
+
+mon1 = StateMonitor(S1, 'w', record=range(4*4))
+spike_mon2 = SpikeMonitor(output)
+
+###########################
+
+load_training_set()
+for i in range(10):
+  # input.rates = training_set[i] * F
+  run(0.25 * second, report='text')
+
+###########################
+
+spike_plot_x0, spike_plot_y0 = spike_plot(spike_mon0)
+spike_plot_x1, spike_plot_y1 = spike_plot(spike_mon1)
+spike_plot_x2, spike_plot_y2 = spike_plot(spike_mon2)
+
+subplot(511)
+plot(spike_plot_x0, spike_plot_y0, '.k')
+
+subplot(512)
+plot(mon0.t/second, mon0.w.T/gmax)
+xlabel('Time (s)')
+ylabel('Weight / gmax')
+
+subplot(513)
+plot(spike_plot_x1, spike_plot_y1, '.k')
+
+subplot(514)
+plot(mon1.t/second, mon1.w.T/0.1)
+xlabel('Time (s)')
+ylabel('Weight / gmax')
+
+subplot(515)
+plot(spike_plot_x2, spike_plot_y2, '.k')
+
+tight_layout()
+show()
+
+
+
+
