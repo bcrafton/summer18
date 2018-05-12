f21a44af9a0bf9b875f4fd19a6cedc710c135e60
Brian Crafton <crafton.b@husky.neu.edu>
2018-05-12 14:42:10 -0400

    small diff but w.e.

--- stdp1layer/stdp-mnist-reduced/training/train.py
+++ stdp1layer/stdp-mnist-reduced/training/train.py
@@ -17,9 +17,6 @@ from struct import unpack
 from brian import *
 import gzip
 
-# specify the location of the MNIST data
-MNIST_data_path = ''
-
 #------------------------------------------------------------------------------ 
 # functions
 #------------------------------------------------------------------------------     
@@ -63,16 +60,12 @@ def get_matrix_from_file(fileName):
 
 def save_connections(ending = ''):
     print 'save connections'
-    for connName in save_conns:
-        connMatrix = connections[connName][:]
-#         connListSparse = ([(i,j[0],j[1]) for i in xrange(connMatrix.shape[0]) for j in zip(connMatrix.rowj[i],connMatrix.rowdata[i])])
-        connListSparse = ([(i,j,connMatrix[i,j]) for i in xrange(connMatrix.shape[0]) for j in xrange(connMatrix.shape[1]) ])
-        np.save(connName + ending, connListSparse)
+    connMatrix = connections['XeAe'][:]
+    connListSparse = ([(i,j,connMatrix[i,j]) for i in xrange(connMatrix.shape[0]) for j in xrange(connMatrix.shape[1]) ])
+    np.save('XeAe' + ending, connListSparse)
 
 def save_theta(ending = ''):
-    print 'save theta'
-    for pop_name in population_names:
-        np.save('theta_' + pop_name + ending, neuron_groups[pop_name + 'e'].theta)
+    np.save('theta_A', neuron_groups['Ae'].theta)
 
 def normalize_weights():
     for connName in connections:
@@ -101,29 +94,6 @@ def get_2d_input_weights():
                     weight_matrix[:, i + j*n_e_sqrt].reshape((n_in_sqrt, n_in_sqrt))
     return rearranged_weights
     
-def get_recognized_number_ranking(assignments, spike_rates):
-    summed_rates = [0] * 10
-    num_assignments = [0] * 10
-    for i in xrange(10):
-        num_assignments[i] = len(np.where(assignments == i)[0])
-        if num_assignments[i] > 0:
-            summed_rates[i] = np.sum(spike_rates[assignments == i]) / num_assignments[i]
-    return np.argsort(summed_rates)[::-1]
-
-def get_new_assignments(result_monitor, input_numbers):
-    assignments = np.zeros(n_e)
-    input_nums = np.asarray(input_numbers)
-    maximum_rate = [0] * n_e    
-    for j in xrange(10):
-        num_assignments = len(np.where(input_nums == j)[0])
-        if num_assignments > 0:
-            rate = np.sum(result_monitor[input_nums == j], axis = 0) / num_assignments
-        for i in xrange(n_e):
-            if rate[i] > maximum_rate[i]:
-                maximum_rate[i] = rate[i]
-                assignments[i] = j
-    return assignments
-    
 #------------------------------------------------------------------------------ 
 # load MNIST
 #------------------------------------------------------------------------------
@@ -150,45 +120,18 @@ b.set_global_preferences(
 
 
 np.random.seed(0)
-data_path = './'
-if test_mode:
-    weight_path = data_path + 'weights/'
-    num_examples = 10000 * 1
-    use_testing_set = True
-    do_plot_performance = False
-    record_spikes = True
-    ee_STDP_on = False
-    update_interval = num_examples
-else:
-    weight_path = data_path + 'random/'  
-    num_examples = 1000
-    use_testing_set = False
-    do_plot_performance = True
-    if num_examples <= 60000:    
-        record_spikes = True
-    else:
-        record_spikes = True
-    ee_STDP_on = True
-
+num_examples = 1000
 
 ending = ''
 n_input = 784
 n_e = 400
 n_i = n_e 
-single_example_time =   0.35 * b.second #
+
+single_example_time =   0.35 * b.second
 resting_time = 0.15 * b.second
+
 runtime = num_examples * (single_example_time + resting_time)
-if num_examples <= 10000:    
-    update_interval = num_examples
-    weight_update_interval = 20
-else:
-    update_interval = 10000
-    weight_update_interval = 100
-if num_examples <= 60000:    
-    save_connections_interval = 10000
-else:
-    save_connections_interval = 10000
-    update_interval = 10000
+update_interval = num_examples
 
 v_rest_e = -65. * b.mV 
 v_rest_i = -60. * b.mV 
@@ -200,17 +143,16 @@ refrac_e = 5. * b.ms
 refrac_i = 2. * b.ms
 
 conn_structure = 'dense'
+
 weight = {}
-delay = {}
-input_population_names = ['X']
-population_names = ['A']
-input_connection_names = ['XA']
-save_conns = ['XeAe']
-input_conn_names = ['ee_input'] 
-recurrent_conn_names = ['ei', 'ie']
 weight['ee_input'] = 78.
+
+delay = {}
 delay['ee_input'] = (0*b.ms,10*b.ms)
 delay['ei_input'] = (0*b.ms,5*b.ms)
+
+NUM_LAYERS = 1
+
 input_intensity = 2.
 start_input_intensity = input_intensity
 
@@ -272,12 +214,10 @@ stdp_methods = {}
 rate_monitors = {}
 spike_monitors = {}
 spike_counters = {}
-result_monitor = np.zeros((update_interval,n_e))
+result_monitor = np.zeros((update_interval, n_e))
 
-neuron_groups['e'] = b.NeuronGroup(n_e*len(population_names), neuron_eqs_e, threshold= v_thresh_e, refractory= refrac_e, reset= scr_e, 
-                                   compile = True, freeze = True)
-neuron_groups['i'] = b.NeuronGroup(n_i*len(population_names), neuron_eqs_i, threshold= v_thresh_i, refractory= refrac_i, reset= v_reset_i, 
-                                   compile = True, freeze = True)
+neuron_groups['e'] = b.NeuronGroup(n_e*NUM_LAYERS, neuron_eqs_e, threshold= v_thresh_e, refractory= refrac_e, reset= scr_e, compile = True, freeze = True)
+neuron_groups['i'] = b.NeuronGroup(n_i*NUM_LAYERS, neuron_eqs_i, threshold= v_thresh_i, refractory= refrac_i, reset= v_reset_i, compile = True, freeze = True)
 
 #------------------------------------------------------------------------------ 
 # create network population and recurrent connections
@@ -324,9 +264,7 @@ stdp_methods['XeAe'] = b.STDP(connections['XeAe'], eqs=eqs_stdp_ee, pre = eqs_st
 #------------------------------------------------------------------------------ 
 
 previous_spike_count = np.zeros(n_e)
-assignments = np.zeros(n_e)
 input_numbers = [0] * num_examples
-outputNumbers = np.zeros((num_examples, 10))
 
 input_groups['Xe'].rate = 0
 
@@ -340,10 +278,7 @@ while j < num_examples:
 
     input_groups['Xe'].rate = rates
     b.run(single_example_time, report='text')
-            
-    if j % update_interval == 0 and j > 0:
-        assignments = get_new_assignments(result_monitor[:], input_numbers[j-update_interval : j])
-    
+
     current_spike_count = np.asarray(spike_counters['Ae'].count[:]) - previous_spike_count
     previous_spike_count = np.copy(spike_counters['Ae'].count[:])
 
@@ -353,10 +288,6 @@ while j < num_examples:
     else:
         result_monitor[j, :] = current_spike_count
         input_numbers[j] = training_labels[j]
-
-        '''
-        outputNumbers[j,:] = get_recognized_number_ranking(assignments, result_monitor[j%update_interval,:])
-        '''
 
         input_intensity = start_input_intensity
         j += 1
@@ -368,47 +299,8 @@ while j < num_examples:
 # save results
 #------------------------------------------------------------------------------ 
 print 'save results'
-if not test_mode:
-    save_theta()
-if not test_mode:
-    save_connections()
-else:
-    np.save(data_path + 'activity/resultPopVecs' + str(num_examples), result_monitor)
-    np.save(data_path + 'activity/inputNumbers' + str(num_examples), input_numbers)
-    
-
-#------------------------------------------------------------------------------ 
-# plot results
-#------------------------------------------------------------------------------ 
-'''
-if rate_monitors:
-    b.figure(fig_num)
-    fig_num += 1
-    for i, name in enumerate(rate_monitors):
-        b.subplot(len(rate_monitors), 1, i)
-        b.plot(rate_monitors[name].times/b.second, rate_monitors[name].rate, '.')
-        b.title('Rates of population ' + name)
-    
-if spike_monitors:
-    b.figure(fig_num)
-    fig_num += 1
-    for i, name in enumerate(spike_monitors):
-        b.subplot(len(spike_monitors), 1, i)
-        b.raster_plot(spike_monitors[name])
-        b.title('Spikes of population ' + name)
-        
-if spike_counters:
-    b.figure(fig_num)
-    fig_num += 1
-    for i, name in enumerate(spike_counters):
-        b.subplot(len(spike_counters), 1, i)
-        b.plot(spike_counters['Ae'].count[:])
-        b.title('Spike count of population ' + name)
-
-plot_2d_input_weights()
-b.ioff()
-b.show()
-'''
+save_theta()
+save_connections()
 
 
 
