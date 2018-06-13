88d473ea47e90e608f4eba103f6472b97419f972
Brian Crafton <crafton.b@husky.neu.edu>
2018-06-13 14:31:16 -0400

    mott works

--- devices/mott.py
+++ devices/mott.py
@@ -1,45 +1,84 @@
 
 import numpy as np
+import pylab as plt
 
 m1 = (1e5 - 5e4) / (0.9 - 0.0)
 m2 = (1e6 - 1e5) / (1.0 - 0.9)
 m3 = (8e5 - 1e6) / (0.1 - 1.0) 
 m4 = (5e4 - 8e5) / (0.0 - 0.1) 
 
-print (m1)
-print (m1 * 0.1)
-print (5e4)
-print (m1 * 0.1 + 5e4)
-
 def mott(r, v):
-    if (v >= 0.0 and r >= 5e4 and r <= 1e5):
+    global state
+    if (v >= 0.0 and v <= 0.9 and state == 0):
         r = m1 * v + 5e4
-        print ("m1")
-    elif (v > 0.9 and v <= 1.0 and r >= 1e5 and r <= 1e6):
+    elif (v > 0.9 and v <= 1.0 and state == 0):
         r = m2 * (v - 0.9) + 1e5
-        print ("m2")
-    elif (v > 0.1 and r > 8e5):
-        r = 1e6 - m3 * v
-        print ("m3")
-    elif (v > 0.0 and r > 5e4):
-        r = 8e5 - m4 * v
-        print ("m4")
+        if (r >= 1e6):
+            state = 1
+        
+    elif (v >= 0.1 and v <= 1.0 and state == 1):
+        r = 1e6 - m3 * (0.9 - v)
+    elif (v >= 0.0 and v < 0.1 and state == 1):
+        r = 8e5 - m4 * (0.1 - v)
+        if (r <= 5e4):
+            state = 0
+        
     else:
         print ("this should never happen")
-        assert(False)
+
     return r, v
-        
+
+vs = []
+rs = []
+
+state = 0
 r = 5e4
 v = 0.01
+
 print (r, v)
-for i in range(100):
-    r, v = mott(r, v)
-    v += 0.01
-    v = np.clip(v, 0, 1.0)
-    print (r, v)
+vs.append(v)
+rs.append(r)
 
 for i in range(100):
     r, v = mott(r, v)
+    
+    vs.append(v)
+    rs.append(r)
+    print (r, v)
+    
+    v += 0.01
+    v = np.clip(v, 0, 1.0)
+
+
+for i in range(100):
+    r, v = mott(r, v)
+    
+    vs.append(v)
+    rs.append(r)
+    print (r, v)
+    
     v -= 0.01
     v = np.clip(v, 0, 1.0)
+    
+for i in range(100):
+    r, v = mott(r, v)
+    
+    vs.append(v)
+    rs.append(r)
     print (r, v)
+    
+    v += 0.01
+    v = np.clip(v, 0, 1.0)
+    
+    
+plt.plot(vs, rs)
+plt.show()
+
+
+
+
+
+
+
+
+
