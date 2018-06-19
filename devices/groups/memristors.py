dd3b462aed2632893cfbc3010e8965e60e143805
Brian Crafton <crafton.b@husky.neu.edu>
2018-06-19 15:38:46 -0400

    memristors

--- devices/groups/memristors.py
+++ devices/groups/memristors.py
@@ -1,38 +1,43 @@
 
-
 import numpy as np
 import pylab as plt
 
 class Memristors:
     
     def __init__(self, N, M):
-        U = 1e-16
-        D = 10e-9
-        W0 = 5e-9
-        RON = 5e4
-        ROFF = 1e6
-        P = 5
+        self.U = 1e-16
+        self.D = 10e-9
+        self.W0 = 5e-9
+        self.RON = 5e4
+        self.ROFF = 1e6
+        self.P = 5            
+        self.W = np.ones(shape=(N, M)) * self.W0
         
-        steps = 1500
-        T = 2 * np.pi
-        dt = T / steps
-        t = 0
-    
-        W = W0
-        Is = np.zeros(shape=(N, M, steps))
-        Vs = np.zeros(shape=(N, M, steps))
-        Rs = np.zeros(shape=(N, M, steps))
-        
-    # this should take a voltage matrix (N, M) input
-    def step(self):
-        # add some randomness here.
-        V = 1 * np.sin(t * dt)
-
-        R = RON * (W / D) + ROFF * (1 - (W / D))
+    def step(self, V, dt):
+        R = self.RON * (self.W / self.D) + self.ROFF * (1 - (self.W / self.D))
         I = V / R
+        F = 1 - (2 * (self.W / self.D) - 1) ** (2 * self.P)
+        dwdt = ((self.U * self.RON * I) / self.D) * F
+        self.W += dwdt * dt
+        
+        return I
+        
+        
+steps = 1500
+T = 2 * np.pi
+dt = T / steps
 
-        F = 1 - (2 * (W / D) - 1) ** (2 * P)
-        dwdt = ((U * RON * I) / D) * F
-        W += dwdt * dt
-        
-        
+M = Memristors(10, 10)
+
+Vs = []
+Is = []
+
+for t in range(steps): 
+    V = np.sin(t * dt) * np.ones(shape=(10, 10))
+    I = M.step(V, dt)
+    
+    Vs.append( V[0, 0] )
+    Is.append( I[0, 0] )
+    
+plt.plot(Vs, Is)
+plt.show()
