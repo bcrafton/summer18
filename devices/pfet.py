5bc2da5cebb9ed6b88b3990044e00fe2fdc40716
Brian Crafton <crafton.b@husky.neu.edu>
2018-06-22 12:19:20 -0400

    think this is right

--- devices/pfet.py
+++ devices/pfet.py
@@ -2,34 +2,61 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def PFET_IDS(VGS_PRE, VDS_PRE):
+def PFET_ISD(VDD, VS, VG, VD):
 
-    VTH_PRE = 0.6
-    K = 20.0
+    VDS = VD - VS
+    VGS = VG - VS
+    
+    VSD = VS - VD
+    VSG = VS - VG
 
-    if (-VGS_PRE >= -VTH_PRE):
-        IDS_PRE = 0
-    elif (0 < -VDS_PRE) and (-VDS_PRE < -VGS_PRE + VTH_PRE):
-        IDS_PRE = -K * ((VGS_PRE - VTH_PRE) * VDS_PRE - VDS_PRE ** 2 / 2)
-    else:
-        IDS_PRE = (-K/2) * (VGS_PRE - VTH_PRE) ** 2
+    VTH = -0.365
+    ABS_VTH = np.absolute(VTH)
+    
+    K = 0.4
+    UT = 0.025
+    I0 = 1e-10
+
+    if (VSG <= ABS_VTH):
+        if (VSD >= 4 * UT):
+            # print ("sub-sat")
+            ISD = I0 * np.exp(K * (VDD - VG - VTH) / UT)
+        else:
+            # print ("sub")
+            # Hasler has (1 - np.exp(-VDD / UT)) because saturation
+            # but we have it a little differently because we not in saturation here.
+            ISD = I0 * np.exp(K * (VDD - VG - VTH) / UT) # * (1 - np.exp(-VDS / UT))
         
-    return IDS_PRE
+    elif (VSG > ABS_VTH) and (VSD <= VSG - ABS_VTH):
+        # print ("linear")
+        ISD = K * (VSG - ABS_VTH) * VSD - VSD ** 2 / 2 
+        
+    elif (VSG > ABS_VTH) and (VSD > VSG - ABS_VTH):
+        # print ("sat")
+        ISD = 0.5 * K * (VSG - ABS_VTH) ** 2
+        
+    else:
+        print "this should never happen"
+        assert(False)
+        
+    return ISD
     
 T = 1
-steps = 1000
+steps = 50
 dt = T / steps
 
-VDS = 2.4
-VGS_SWEEP = np.linspace(0, 2.4, steps)
+VDD = 1.0
+VS = VDD
+VG_SWEEP = np.linspace(0.0, 1.0, steps)
+VD = 0.320
 
-ids_out = []
+IDS_OUT = []
 
 for step in range(steps):
     t = step * dt
-    VGS = VGS_SWEEP[step]
-    ids = PFET_IDS(VGS, VDS)
-    ids_out.append(ids)
+    VG = VG_SWEEP[step]
+    IDS = PFET_ISD(VDD, VS, VG, VD)
+    IDS_OUT.append(IDS)
 
-plt.plot(VGS_SWEEP, ids_out)
+plt.plot(VG_SWEEP, IDS_OUT)
 plt.show()
