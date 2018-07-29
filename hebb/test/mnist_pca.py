ae792090b21723053568a92ec9c240b989148e97
Brian Crafton <crafton.b@husky.neu.edu>
2018-07-29 14:57:43 -0400

    yellow figure thing

--- hebb/test/mnist_pca.py
+++ hebb/test/mnist_pca.py
@@ -69,47 +69,43 @@ pca.fit(training_set)
 
 w = np.absolute(np.random.normal(0.1, 0.05, size=(layer1, layer2)))
 
-NUM_ITRS = 3
+NUM_ITRS = 5
 NUM_TRAIN_EXAMPLES = 50000
 NUM_TEST_EXAMPLES = 10000
 
 for itr in range(NUM_ITRS):
   print itr
   for i in range(NUM_TRAIN_EXAMPLES):
-    # x = np.array(training_set[i]).reshape(layer1)
-    # x = x / np.average(x)
     x = training_set[i]
     y = np.dot(x, w)
-    # y = y / np.max(y)
     wy = np.dot(w, y)
     d = x - wy
-    # w = w + 1e-5 * d.reshape(784, 1) * y.reshape(1, 400)
-    w = np.clip(w + 1e-5 * d.reshape(784, 1) * y.reshape(1, 400), 0, 1.0)
+
+    # we need to use 1e-4 bc sometimes col_norm actually goes to 0.    
+    # that seems like a problem ...
+    w = np.clip(w + 1e-8 * d.reshape(784, 1) * y.reshape(1, 400), 1e-6, 1.0)
+    
+    if (i % 100 == 0):
+      col_norm = np.average(w, axis = 0)
+      if (np.count_nonzero(col_norm) < 400):
+        print col_norm
+      col_norm = 0.1 / col_norm
+      for j in range(layer2):
+        w[:, j] *= col_norm[j]
   
 print np.std(w), np.average(w), np.max(w), np.min(w)
   
 train_spks = np.zeros(shape=(NUM_TRAIN_EXAMPLES, layer2))
 for i in range(NUM_TRAIN_EXAMPLES):
-  # x = np.array(training_set[i]).reshape(layer1)
-  # x = x / np.average(x)
   x = training_set[i]
   y = np.dot(x, w)
-  # y = y / np.max(y)
   train_spks[i] = y
   
 test_spks = np.zeros(shape=(NUM_TEST_EXAMPLES, layer2))
 for i in range(NUM_TEST_EXAMPLES):
-  # x = np.array(testing_set[i]).reshape(layer1)
-  # x = x / np.average(x)
   x = testing_set[i]
   y = np.dot(x, w)
-  # y = y / np.max(y)
   test_spks[i] = y
-
-# how do we do pca transform on these things ? 
-# i guess we just use spike counts ?
-# that really is our transform ...
-# yeah and just turn learning off for the testing set.
 
 #####################################
 
