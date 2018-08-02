b93cd8b4ea81e4c57d18e332d7fbf569fcf1475f
Brian Crafton <crafton.b@husky.neu.edu>
2018-08-02 00:03:22 -0400

    script fix

--- khan/iedm/run.sh
+++ khan/iedm/run.sh
@@ -1,8 +1,8 @@
-python 3k.py;
-python 5k.py;
-python 15k.py;
-python 20k.py;
-python 1k.py;
-python 10k.py;
-python 50k.py;
-python 150k.py; 
+python 3k.py &
+python 5k.py &
+python 15k.py &
+python 20k.py &
+python 1k.py &
+python 10k.py &
+python 50k.py &
+python 150k.py &
