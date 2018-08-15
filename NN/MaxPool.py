05478171c1790d0c2bc2bae5b3b01758f8a73ed8
Brian Crafton <crafton.b@husky.neu.edu>
2018-08-15 09:57:34 -0400

    max pool sorta working

--- NN/MaxPool.py
+++ NN/MaxPool.py
@@ -2,7 +2,7 @@
 import tensorflow as tf
 import numpy as np
 import math
-# from tensorflow.python.ops import gen_nn_ops
+from tensorflow.python.ops import gen_nn_ops
 # return gen_nn_ops.max_pool_v2(value=X, ksize=self.size, strides=self.stride, padding="SAME")
 
 from Layer import Layer 
@@ -18,13 +18,15 @@ class MaxPool(Layer):
         return tf.random_uniform(shape=(1, 1))
 
     def forward(self, X: np.ndarray):
-        return tf.nn.max_pool(value=X, ksize=self.size, strides=self.stride, padding="SAME")
+        # return tf.nn.max_pool(value=X, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
+        return gen_nn_ops.max_pool_v2(X, [1, 2, 2, 1], [1, 2, 2, 1], padding="SAME")
     
     def backward(self, AIN : np.ndarray, AOUT : np.ndarray, DIN : np.ndarray):
         # think both of these would work
         
         # return tf.multiply(tf.cast(AOUT > 0.0, dtype=tf.float32), 1.0)
         
-        pool = tf.nn.max_pool(value=AIN, ksize=self.size, strides=self.stride, padding="SAME")
-        return tf.multiply(tf.cast(pool > 0.0, dtype=tf.float32), 1.0)
-        
+        grad = gen_nn_ops.max_pool_grad_v2(grad=DIN, orig_input=AIN, orig_output=AOUT, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
+        # pool, argmax = tf.nn.max_pool_with_argmax(AIN, self.size, self.stride, padding="SAME")
+        # return tf.multiply(tf.cast(pool > 0.0, dtype=tf.float32), 1.0)
+        return grad
