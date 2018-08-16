23e6da63792df5990857b02e4e28333b5e23b26e
Brian A Crafton <bcrafton3@KL2125-02.ece.gatech.edu>
2018-08-15 19:38:53 -0700

    cifar10 updateS'

--- NN/Cifar10.py
+++ NN/Cifar10.py
@@ -31,8 +31,8 @@ cifar10 = tf.keras.datasets.cifar10.load_data()
 EPOCHS = 10
 TRAIN_EXAMPLES = 50000
 TEST_EXAMPLES = 10000
-BATCH_SIZE = 10
-ALPHA = 1e-3
+BATCH_SIZE = 25
+ALPHA = 1e-2
 
 ##############################################
 
@@ -49,20 +49,22 @@ l0 = Convolution(input_sizes=[batch_size, 32, 32, 3], filter_sizes=[5, 5, 3, 96]
 W1 = tf.Variable(tf.random_uniform(shape=[5, 5, 96, 128]) * (2 * 0.12) - 0.12)
 l1 = Convolution(input_sizes=[batch_size, 32, 32, 96], filter_sizes=[5, 5, 96, 128], filters=W1, stride=1, padding=1, alpha=ALPHA, activation=Relu(), last_layer=False)
 
-W2 = tf.Variable(tf.random_uniform(shape=[5, 5, 128, 256]) * (2 * 0.12) - 0.12)
-l2 = Convolution(input_sizes=[batch_size, 32, 32, 128], filter_sizes=[5, 5, 128, 256], filters=W2, stride=1, padding=1, alpha=ALPHA, activation=Relu(), last_layer=False)
+l2 = MaxPool(size=[batch_size, 32, 32, 128], stride=[1, 2, 2, 1])
 
-l3 = MaxPool(size=[batch_size, 32, 32, 256], stride=[1, 2, 2, 1])
+W3 = tf.Variable(tf.random_uniform(shape=[5, 5, 128, 256]) * (2 * 0.12) - 0.12)
+l3 = Convolution(input_sizes=[batch_size, 16, 16, 128], filter_sizes=[5, 5, 128, 256], filters=W3, stride=1, padding=1, alpha=ALPHA, activation=Relu(), last_layer=False)
 
-l4 = ConvToFullyConnected(shape=[16, 16, 256])
+l4 = MaxPool(size=[batch_size, 16, 16, 256], stride=[1, 2, 2, 1])
 
-W5 = tf.Variable(tf.random_uniform(shape=[16*16*256, 1024]) * (2 * 0.12) - 0.12)
-l5 = FullyConnected(size=[16*16*256, 1024], weights=W5, alpha=ALPHA, activation=Relu(), last_layer=False)
+l5 = ConvToFullyConnected(shape=[8, 8, 256])
 
-W6 = tf.Variable(tf.random_uniform(shape=[1024, 10]) * (2 * 0.12) - 0.12)
-l6 = FullyConnected(size=[1024, 10], weights=W6, alpha=ALPHA, activation=Relu(), last_layer=False)
+W6 = tf.Variable(tf.random_uniform(shape=[8*8*256, 2048]) * (2 * 0.12) - 0.12)
+l6 = FullyConnected(size=[8*8*256, 2048], weights=W6, alpha=ALPHA, activation=Relu(), last_layer=False)
 
-model = Model(layers=[l0, l1, l2, l3, l4, l5, l6])
+W7 = tf.Variable(tf.random_uniform(shape=[2048, 10]) * (2 * 0.12) - 0.12)
+l7 = FullyConnected(size=[2048, 10], weights=W7, alpha=ALPHA, activation=Relu(), last_layer=False)
+
+model = Model(layers=[l0, l1, l2, l3, l4, l5, l6, l7])
 
 ##############################################
 
@@ -78,6 +80,7 @@ acc, acc_op = tf.metrics.accuracy(labels=tf.argmax(Y, 1), predictions=tf.argmax(
 
 sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
+tf.local_variables_initializer().run()
 
 (x_train, y_train), (x_test, y_test) = cifar10
 
@@ -91,14 +94,16 @@ y_test = keras.utils.to_categorical(y_test, 10)
 
 for ii in range(EPOCHS):
     for jj in range(0, TRAIN_EXAMPLES, BATCH_SIZE):
+        print (str(ii * TRAIN_EXAMPLES + jj) + "/" + str(EPOCHS * TRAIN_EXAMPLES))
+
         start = jj % TRAIN_EXAMPLES
         end = jj % TRAIN_EXAMPLES + BATCH_SIZE
         sess.run([ret], feed_dict={batch_size: BATCH_SIZE, X: x_train[start:end], Y: y_train[start:end]})
 
 for jj in range(0, TEST_EXAMPLES, BATCH_SIZE):
-    start = jj % TRAIN_EXAMPLES
-    end = jj % TRAIN_EXAMPLES + BATCH_SIZE
-    print('acc:', sess.run(acc_op, feed_dict={batch_size: BATCH_SIZE, X: batch_xs, Y: batch_ys}))
+    start = jj % TEST_EXAMPLES
+    end = jj % TEST_EXAMPLES + BATCH_SIZE
+    print('acc:', sess.run(acc_op, feed_dict={batch_size: BATCH_SIZE, X: x_test[start:end], Y: y_test[start:end]}))
 
 
 
