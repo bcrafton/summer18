8f870933be75ba874bde619a5edf95c72d54e9dc
Brian Crafton <crafton.b@husky.neu.edu>
2018-08-18 16:33:32 -0400

    softmax

--- NN/Activation.py
+++ NN/Activation.py
@@ -32,8 +32,10 @@ class Relu(Activation):
         return tf.nn.relu(x)
 
     def gradient(self, x):
+        # pretty sure this gradient works for A and Z
         return tf.cast(x > 0.0, dtype=tf.float32)
-        
+
+# https://theclevermachine.wordpress.com/tag/tanh-function/ 
 class Tanh(Activation):
 
     def __init__(self):
@@ -43,4 +45,32 @@ class Tanh(Activation):
         return tf.tanh(x)
 
     def gradient(self, x: np.ndarray):
+        # this is gradient wtf A, not Z
         return 1 - tf.pow(x, 2)
+        
+# https://medium.com/@aerinykim/how-to-implement-the-softmax-derivative-independently-from-any-loss-function-ae6d44363a9d
+# /home/brian/tensorflow/tensorflow/python/ops/nn_grad ... grep "_SoftmaxGrad"
+
+class Softmax(Activation):
+    def __init__(self):
+        pass
+
+    def forward(self, x: np.ndarray):
+        return tf.softmax(x)
+
+    # this is gradient for A
+    def gradient(self, x: np.ndarray):
+        flat = tf.reshape(x, [-1])
+        diagflat = tf.diag(flat)
+        dot = tf.matmul(flat, tf.transpose(flat))
+        return diagflag - dot
+        
+        
+        
+                
+        
+        
+        
+        
+        
+        
