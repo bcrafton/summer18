6fd465a5a4dcf345a567e3d84db4d9538815006d
Brian Crafton <crafton.b@husky.neu.edu>
2018-08-23 17:29:24 -0400

    using linear output activation and using VALID maxpool

--- NN/Cifar10_large.py
+++ NN/Cifar10_large.py
@@ -6,7 +6,7 @@ import sys
 ##############################################
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--epochs', type=int, default=10000)
+parser.add_argument('--epochs', type=int, default=300)
 parser.add_argument('--batch_size', type=int, default=64)
 parser.add_argument('--alpha', type=float, default=5e-5)
 parser.add_argument('--gpu', type=int, default=0)
@@ -45,6 +45,7 @@ from Activation import Relu
 from Activation import Tanh
 from Activation import Softmax
 from Activation import LeakyRelu
+from Activation import Linear
 
 ##############################################
 
@@ -75,30 +76,24 @@ YTEST = tf.placeholder(tf.float32, [None, 10])
 XTEST = tf.map_fn(lambda frame1: tf.image.per_image_standardization(frame1), XTEST)
 
 l0 = Convolution(input_sizes=[batch_size, 32, 32, 3], filter_sizes=[5, 5, 3, 96], num_classes=10, init_filters=args.init, strides=[1, 1, 1, 1], padding="SAME", alpha=ALPHA, activation=Tanh(), last_layer=False, sparse=sparse)
+l1 = MaxPool(size=[batch_size, 32, 32, 96], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="VALID")
+l2 = Feedback(size=[batch_size, 15, 15, 96], num_classes=10, sparse=False)
 
-l1 = MaxPool(size=[batch_size, 32, 32, 96], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="SAME")
+l3 = Convolution(input_sizes=[batch_size, 15, 15, 96], filter_sizes=[5, 5, 96, 128], num_classes=10, init_filters=args.init, strides=[1, 1, 1, 1], padding="SAME", alpha=ALPHA, activation=Tanh(), last_layer=False, sparse=sparse)
+l4 = MaxPool(size=[batch_size, 15, 15, 128], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="VALID")
+l5 = Feedback(size=[batch_size, 7, 7, 128], num_classes=10, sparse=False)
 
-l2 = Feedback(size=[batch_size, 16, 16, 96], num_classes=10, sparse=False)
+l6 = Convolution(input_sizes=[batch_size, 7, 7, 128], filter_sizes=[5, 5, 128, 256], num_classes=10, init_filters=args.init, strides=[1, 1, 1, 1], padding="SAME", alpha=ALPHA, activation=Tanh(), last_layer=False, sparse=sparse)
+l7 = MaxPool(size=[batch_size, 7, 7, 256], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="VALID")
+l8 = Feedback(size=[batch_size, 3, 3, 256], num_classes=10, sparse=False)
 
-l3 = Convolution(input_sizes=[batch_size, 16, 16, 96], filter_sizes=[5, 5, 96, 128], num_classes=10, init_filters=args.init, strides=[1, 1, 1, 1], padding="SAME", alpha=ALPHA, activation=Tanh(), last_layer=False, sparse=sparse)
+l9 = ConvToFullyConnected(shape=[3, 3, 256])
 
-l4 = MaxPool(size=[batch_size, 16, 16, 128], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="SAME")
-
-l5 = Feedback(size=[batch_size, 8, 8, 128], num_classes=10, sparse=False)
-
-l6 = Convolution(input_sizes=[batch_size, 8, 8, 128], filter_sizes=[5, 5, 128, 256], num_classes=10, init_filters=args.init, strides=[1, 1, 1, 1], padding="SAME", alpha=ALPHA, activation=Tanh(), last_layer=False, sparse=sparse)
-
-l7 = MaxPool(size=[batch_size, 8, 8, 256], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="SAME")
-
-l8 = Feedback(size=[batch_size, 4, 4, 256], num_classes=10, sparse=False)
-
-l9 = ConvToFullyConnected(shape=[4, 4, 256])
-
-l10 = FullyConnected(size=[4*4*256, 2048], num_classes=10, init_weights=args.init, alpha=ALPHA, activation=Tanh(), last_layer=False, sparse=sparse)
+l10 = FullyConnected(size=[3*3*256, 2048], num_classes=10, init_weights=args.init, alpha=ALPHA, activation=Tanh(), last_layer=False, sparse=sparse)
 
 l11 = FullyConnected(size=[2048, 2048], num_classes=10, init_weights=args.init, alpha=ALPHA, activation=Tanh(), last_layer=False, sparse=sparse)
 
-l12 = FullyConnected(size=[2048, 10], num_classes=10, init_weights=args.init, alpha=ALPHA, activation=Tanh(), last_layer=True, sparse=sparse)
+l12 = FullyConnected(size=[2048, 10], num_classes=10, init_weights=args.init, alpha=ALPHA, activation=Linear(), last_layer=True, sparse=sparse)
 
 model = Model(layers=[l0, l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12])
 
