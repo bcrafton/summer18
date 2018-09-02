b04999758af1a708161b3c2622f9bd57f0cf6066
Brian Crafton <crafton.b@husky.neu.edu>
2018-09-02 10:28:40 -0400

    put waits in here

--- NN/test_rank_sparse.sh
+++ NN/test_rank_sparse.sh
@@ -3,10 +3,14 @@ python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 1
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 1 --rank 3 --init zero --opt adam --gpu 2 > sparse1rank3 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 1 --rank 4 --init zero --opt adam --gpu 3 > sparse1rank4 &
 
+wait
+
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 1 --rank 5 --init zero --opt adam --gpu 0 > sparse1rank5 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 1 --rank 6 --init zero --opt adam --gpu 1 > sparse1rank6 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 1 --rank 7 --init zero --opt adam --gpu 2 > sparse1rank7 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 1 --rank 8 --init zero --opt adam --gpu 3 > sparse1rank8 &
+
+wait
 
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 1 --rank 9 --init zero --opt adam --gpu 0 > sparse1rank9 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 1 --rank 10 --init zero --opt adam --gpu 1 > sparse1rank10 &
@@ -18,10 +22,14 @@ python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 2
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 2 --rank 3 --init zero --opt adam --gpu 2 > sparse2rank3 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 2 --rank 4 --init zero --opt adam --gpu 3 > sparse2rank4 &
 
+wait
+
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 2 --rank 5 --init zero --opt adam --gpu 0 > sparse2rank5 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 2 --rank 6 --init zero --opt adam --gpu 1 > sparse2rank6 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 2 --rank 7 --init zero --opt adam --gpu 2 > sparse2rank7 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 2 --rank 8 --init zero --opt adam --gpu 3 > sparse2rank8 &
+
+wait
 
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 2 --rank 9 --init zero --opt adam --gpu 0 > sparse2rank9 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 2 --rank 10 --init zero --opt adam --gpu 1 > sparse2rank10 &
@@ -33,10 +41,14 @@ python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 2
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 3 --rank 3 --init zero --opt adam --gpu 2 > sparse3rank3 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 3 --rank 4 --init zero --opt adam --gpu 3 > sparse3rank4 &
 
+wait
+
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 3 --rank 5 --init zero --opt adam --gpu 0 > sparse3rank5 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 3 --rank 6 --init zero --opt adam --gpu 1 > sparse3rank6 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 3 --rank 7 --init zero --opt adam --gpu 2 > sparse3rank7 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 3 --rank 8 --init zero --opt adam --gpu 3 > sparse3rank8 &
+
+wait
 
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 3 --rank 9 --init zero --opt adam --gpu 0 > sparse3rank9 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 3 --rank 10 --init zero --opt adam --gpu 1 > sparse3rank10 &
@@ -49,10 +61,14 @@ python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 3
 # python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 4 --rank 3 --init zero --opt adam --gpu 2 > sparse4rank3 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 4 --rank 4 --init zero --opt adam --gpu 3 > sparse4rank4 &
 
+wait
+
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 4 --rank 5 --init zero --opt adam --gpu 0 > sparse4rank5 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 4 --rank 6 --init zero --opt adam --gpu 1 > sparse4rank6 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 4 --rank 7 --init zero --opt adam --gpu 2 > sparse4rank7 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 4 --rank 8 --init zero --opt adam --gpu 3 > sparse4rank8 &
+
+wait
 
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 4 --rank 9 --init zero --opt adam --gpu 0 > sparse4rank9 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 4 --rank 10 --init zero --opt adam --gpu 1 > sparse4rank10 &
@@ -69,6 +85,8 @@ python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 5
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 5 --rank 7 --init zero --opt adam --gpu 2 > sparse5rank7 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 5 --rank 8 --init zero --opt adam --gpu 3 > sparse5rank8 &
 
+wait
+
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 5 --rank 9 --init zero --opt adam --gpu 0 > sparse5rank9 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 5 --rank 10 --init zero --opt adam --gpu 1 > sparse5rank10 &
 
@@ -84,6 +102,8 @@ python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 6
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 6 --rank 7 --init zero --opt adam --gpu 2 > sparse6rank7 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 6 --rank 8 --init zero --opt adam --gpu 3 > sparse6rank8 &
 
+wait
+
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 6 --rank 9 --init zero --opt adam --gpu 0 > sparse6rank9 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 6 --rank 10 --init zero --opt adam --gpu 1 > sparse6rank10 &
 
@@ -98,6 +118,8 @@ python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 6
 # python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 7 --rank 6 --init zero --opt adam --gpu 1 > sparse7rank6 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 7 --rank 7 --init zero --opt adam --gpu 2 > sparse7rank7 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 7 --rank 8 --init zero --opt adam --gpu 3 > sparse7rank8 &
+
+wait
 
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 7 --rank 9 --init zero --opt adam --gpu 0 > sparse7rank9 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 7 --rank 10 --init zero --opt adam --gpu 1 > sparse7rank10 &
@@ -117,6 +139,8 @@ python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 8
 
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 8 --rank 9 --init zero --opt adam --gpu 0 > sparse8rank9 &
 python mnist_fc.py --epochs 200 --batch_size 32 --alpha 0.005 --dfa 1 --sparse 8 --rank 10 --init zero --opt adam --gpu 1 > sparse8rank10 &
+
+wait
 
 #######################################
 
