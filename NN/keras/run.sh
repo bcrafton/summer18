4cf963f4341919bba9b93170f40f6e85d6ea9bf7
Brian A Crafton <bcrafton3@KL2125-02.ece.gatech.edu>
2018-09-02 18:12:49 -0700

    dump to dirs

--- NN/keras/run.sh
+++ NN/keras/run.sh
@@ -1,8 +1,8 @@
 
-python keras_mnist_fc.py      --gpu 0 --alpha 0.01  --epochs 100 > keras_mnist_fc &
-python keras_mnist_conv.py    --gpu 0 --alpha 0.001 --epochs 100 > keras_mnist_conv &
-python keras_cifar10_fc.py    --gpu 1 --alpha 0.01  --epochs 100 > keras_cifar10_fc &
-python keras_cifar10_conv.py  --gpu 2 --alpha 0.001 --epochs 100 > keras_cifar10_conv &
-python keras_cifar100_fc.py   --gpu 1 --alpha 0.01  --epochs 100 > keras_cifar100_fc &
-python keras_cifar100_conv.py --gpu 3 --alpha 0.001 --epochs 100 > keras_cifar100_conv &
+python keras_mnist_fc.py      --gpu 0 --alpha 0.01  --epochs 100 > ./res/keras_mnist_fc &
+python keras_mnist_conv.py    --gpu 0 --alpha 0.001 --epochs 100 > ./res/keras_mnist_conv &
+python keras_cifar10_fc.py    --gpu 1 --alpha 0.01  --epochs 100 > ./res/keras_cifar10_fc &
+python keras_cifar10_conv.py  --gpu 2 --alpha 0.001 --epochs 100 > ./res/keras_cifar10_conv &
+python keras_cifar100_fc.py   --gpu 1 --alpha 0.01  --epochs 100 > ./res/keras_cifar100_fc &
+python keras_cifar100_conv.py --gpu 3 --alpha 0.001 --epochs 100 > ./res/keras_cifar100_conv &
 
--- NN/tf/run.sh
+++ NN/tf/run.sh
@@ -1,7 +1,7 @@
 
-python tf_mnist_fc.py      --gpu 0 --alpha 0.01  --epochs 100 > tf_mnist_fc &
-python tf_mnist_conv.py    --gpu 0 --alpha 0.001 --epochs 100 > tf_mnist_conv &
-python tf_cifar10_fc.py    --gpu 0 --alpha 0.01  --epochs 100 > tf_cifar10_fc &
-python tf_cifar10_conv.py  --gpu 0 --alpha 0.001 --epochs 100 > tf_cifar10_conv &
-python tf_cifar100_fc.py   --gpu 0 --alpha 0.01  --epochs 100 > tf_cifar100_fc &
-python tf_cifar100_conv.py --gpu 0 --alpha 0.001 --epochs 100 > tf_cifar100_conv &
+python tf_mnist_fc.py      --gpu 0 --alpha 0.01  --epochs 100 > ./res/tf_mnist_fc &
+python tf_mnist_conv.py    --gpu 0 --alpha 0.001 --epochs 100 > ./res/tf_mnist_conv &
+python tf_cifar10_fc.py    --gpu 0 --alpha 0.01  --epochs 100 > ./res/tf_cifar10_fc &
+python tf_cifar10_conv.py  --gpu 0 --alpha 0.001 --epochs 100 > ./res/tf_cifar10_conv &
+python tf_cifar100_fc.py   --gpu 0 --alpha 0.01  --epochs 100 > ./res/tf_cifar100_fc &
+python tf_cifar100_conv.py --gpu 0 --alpha 0.001 --epochs 100 > ./res/tf_cifar100_conv &
