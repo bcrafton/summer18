8378025d5c60bdb3a2d396e74153a81e013c8e6e
Brian Crafton <crafton.b@husky.neu.edu>
2018-09-10 11:03:51 -0400

    to collect 10 samples

---
+++ NN/rank_sparse.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+for i in {1..10}
+do
+    for (( j=$i; j<=10; j++ ))
+    do
+        for k in {1..10}
+        do
+            fname=sparse$i
+            fname+=rank$j
+            fname+=Itr$k
+            # echo $fname
+            python mnist_fc.py --epochs 500 --batch_size 32 --alpha 0.005 --dfa 1 --sparse $i --rank $j --init zero --opt adam --gpu 0 --name $fname &
+        done
+        wait
+    done
+done
