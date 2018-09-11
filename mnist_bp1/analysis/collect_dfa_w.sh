a5834f8b4c3afdedf1f21766587c97d0687249e4
Brian A Crafton <bcrafton3@KL2125-02.ece.gatech.edu>
2018-09-11 15:42:17 -0700

    bad and good init results

--- mnist_bp1/analysis/collect_dfa_w.sh
+++ mnist_bp1/analysis/collect_dfa_w.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-for i in {1..2500}
+for i in {1..250}
 do
    python mnist_dfa.py --gpu 0 --num $i &
    python mnist_dfa.py --gpu 1 --num $i &
    python mnist_dfa.py --gpu 2 --num $i &
    python mnist_dfa.py --gpu 3 --num $i &
+
+   python mnist_dfa_bad.py --gpu 0 --num $i &
+   python mnist_dfa_bad.py --gpu 1 --num $i &
+   python mnist_dfa_bad.py --gpu 2 --num $i &
+   python mnist_dfa_bad.py --gpu 3 --num $i &
    wait
 done
---
+++ mnist_bp1/analysis/mnist_dfa_bad.py
@@ -0,0 +1,135 @@
+
+import os
+import argparse 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--num', type=int, default=0)
+parser.add_argument('--gpu', type=int, default=0)
+args = parser.parse_args()
+
+if args.gpu >= 0:
+    os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"]=str(args.gpu)
+
+import tensorflow as tf
+import numpy as np
+import keras
+from keras.datasets import mnist
+
+##############################################
+
+TRAIN_EXAMPLES = 60000
+TEST_EXAMPLES = 10000
+NUM_CLASSES = 10
+EPOCHS = 50
+BATCH_SIZE = 32
+
+##############################################
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(TRAIN_EXAMPLES, 784)
+x_test = x_test.reshape(TEST_EXAMPLES, 784)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+y_train = keras.utils.to_categorical(y_train, NUM_CLASSES)
+y_test = keras.utils.to_categorical(y_test, NUM_CLASSES)
+
+##############################################
+
+def add_bias(x):
+    return tf.concat([x, tf.ones([tf.shape(x)[0], 1])], axis=1)
+  
+def relu(x):
+    return tf.nn.relu(x)
+
+def drelu(x):
+    return tf.cast(x > 0.0, dtype=tf.float32)
+    
+def sigmoid(x):
+    return tf.sigmoid(x)
+
+def dsigmoid(x):
+    return tf.multiply(x, tf.subtract(1.0, x))
+
+##############################################
+high = 1.0 / np.sqrt(785)
+low = -high
+W1 = tf.Variable(tf.random_uniform(minval=low, maxval=high, shape=[785, 25]))
+
+high = 1.0 / np.sqrt(26)
+low = -high
+W2 = tf.Variable(tf.random_uniform(minval=low, maxval=high, shape=[26, 10]))
+
+B = tf.Variable(np.load("BAD_B.npy"), dtype=tf.float32)
+##############################################
+# FEED FORWARD
+##############################################
+ALPHA = tf.placeholder(tf.float32, shape=())
+X = tf.placeholder(tf.float32, [None, 784])
+A1 = add_bias(X)
+
+Y2 = tf.matmul(A1, W1)
+A2 = add_bias(sigmoid(Y2))
+
+Y3 = tf.matmul(A2, W2)
+A3 = sigmoid(Y3)
+##############################################
+# BACK PROP
+##############################################
+ANS = tf.placeholder(tf.float32, [None, 10])
+D3 = tf.subtract(A3, ANS)
+D2 = tf.multiply(tf.matmul(D3, tf.transpose(B)), dsigmoid(A2))
+
+G2 = tf.matmul(tf.transpose(A2), D3)
+G1 = tf.matmul(tf.transpose(A1), D2[:, :-1])
+
+W2 = W2.assign(tf.subtract(W2, tf.scalar_mul(ALPHA, G2)))
+W1 = W1.assign(tf.subtract(W1, tf.scalar_mul(ALPHA, G1)))
+
+correct_prediction = tf.equal(tf.argmax(A3,1), tf.argmax(ANS,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+##############################################
+
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+
+prev1 = np.zeros(shape=(785, 25))
+prev2 = np.zeros(shape=(26, 10))
+
+for ii in range(EPOCHS):
+    for jj in range(int(TRAIN_EXAMPLES / BATCH_SIZE)):
+        xs = x_train[jj*BATCH_SIZE:(jj+1)*BATCH_SIZE]
+        ys = y_train[jj*BATCH_SIZE:(jj+1)*BATCH_SIZE]
+        sess.run([W1, W2], feed_dict={ALPHA: 0.01, X: xs, ANS: ys})
+        
+    total_correct_examples = 0.0
+    total_examples = 0.0
+
+    acc, w1, w2 = sess.run([accuracy, W1, W2], feed_dict={ALPHA: 0.00, X: x_test, ANS: y_test})
+    
+    # print (np.sum(np.absolute(prev1 - w1)))
+    # print (np.sum(np.absolute(prev2 - w2)))
+    # prev1 = w1
+    # prev2 = w2
+    
+    print ("acc: " + str(acc))
+
+np.save("BAD_W1_" + str(args.num) + "_" + str(args.gpu), w1)
+np.save("BAD_W2_" + str(args.num) + "_" + str(args.gpu), w2)
+
+print ("accuracy: " + str(acc))
+
+
+
+
+
+
+
+
+
+
+
