591d09d5d231200937b88d4e860d2c2df3b2c479
Brian Crafton <crafton.b@husky.neu.edu>
2018-09-13 13:13:14 -0400

    run this on gpu

--- NN/collect_dfa_w.sh
+++ NN/collect_dfa_w.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 for i in {1..250}
 do
-   python mnist_fc.py --dfa 1 --epochs 100 --save 1 --gpu 0 --num $i &
-   #python mnist_dfa.py --gpu 1 --num $i &
-   #python mnist_dfa.py --gpu 2 --num $i &
-   #python mnist_dfa.py --gpu 3 --num $i &
-
-   #python mnist_dfa_bad.py --gpu 0 --num $i &
-   #python mnist_dfa_bad.py --gpu 1 --num $i &
-   #python mnist_dfa_bad.py --gpu 2 --num $i &
-   #python mnist_dfa_bad.py --gpu 3 --num $i &
+   python mnist_fc.py --epochs 100 --batch_size 32 --alpha 0.01 --gpu 0 --dfa 1 --sparse 0 --rank 0 --init sqrt_fan_in --opt adam --save 1 --num $i --load B.npy &
+   
+   python mnist_fc.py --epochs 100 --batch_size 32 --alpha 0.01 --gpu 1 --dfa 1 --sparse 0 --rank 0 --init sqrt_fan_in --opt adam --save 1 --num $i --load BAD_B.npy &
+   
+   python mnist_fc.py --epochs 100 --batch_size 32 --alpha 0.01 --gpu 2 --dfa 1 --sparse 0 --rank 0 --init sqrt_fan_in --opt adam --save 1 --num $i --load SPARSE_B.npy &
+   
+   python mnist_fc.py --epochs 100 --batch_size 32 --alpha 0.01 --gpu 3 --dfa 1 --sparse 0 --rank 0 --init sqrt_fan_in --opt adam --save 1 --num $i --load SPARSE_BAD_B.npy &
+   
    wait
 done
