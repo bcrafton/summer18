5b0026f6bf9296c0083e1ff19d9917bcde4b2343
Brian Crafton <crafton.b@husky.neu.edu>
2018-09-14 10:48:12 -0400

    fix this script

--- NN/collect_dfa_w.sh
+++ NN/collect_dfa_w.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 for i in {1..250}
 do
-   python mnist_fc.py --epochs 100 --batch_size 32 --alpha 0.01 --gpu 0 --dfa 1 --sparse 0 --rank 0 --init sqrt_fan_in --opt adam --save 1 --num $i --load B.npy &
+   python mnist_fc.py --epochs 100 --batch_size 32 --alpha 0.01 --gpu 0 --dfa 1 --sparse 0 --rank 0 --init sqrt_fan_in --opt adam --save 1 --num $i --load ./analysis/random_feedback/B.npy &
    
-   python mnist_fc.py --epochs 100 --batch_size 32 --alpha 0.01 --gpu 1 --dfa 1 --sparse 0 --rank 0 --init sqrt_fan_in --opt adam --save 1 --num $i --load BAD_B.npy &
+   python mnist_fc.py --epochs 100 --batch_size 32 --alpha 0.01 --gpu 1 --dfa 1 --sparse 0 --rank 0 --init sqrt_fan_in --opt adam --save 1 --num $i --load ./analysis/random_feedback/BAD_B.npy &
    
-   python mnist_fc.py --epochs 100 --batch_size 32 --alpha 0.01 --gpu 2 --dfa 1 --sparse 0 --rank 0 --init sqrt_fan_in --opt adam --save 1 --num $i --load SPARSE_B.npy &
+   python mnist_fc.py --epochs 100 --batch_size 32 --alpha 0.01 --gpu 2 --dfa 1 --sparse 0 --rank 0 --init sqrt_fan_in --opt adam --save 1 --num $i --load ./analysis/random_feedback/SPARSE_B.npy &
    
-   python mnist_fc.py --epochs 100 --batch_size 32 --alpha 0.01 --gpu 3 --dfa 1 --sparse 0 --rank 0 --init sqrt_fan_in --opt adam --save 1 --num $i --load SPARSE_BAD_B.npy &
+   python mnist_fc.py --epochs 100 --batch_size 32 --alpha 0.01 --gpu 3 --dfa 1 --sparse 0 --rank 0 --init sqrt_fan_in --opt adam --save 1 --num $i --load ./analysis/random_feedback/SPARSE_BAD_B.npy &
    
    wait
 done
